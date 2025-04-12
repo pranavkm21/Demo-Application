6ec13c4a258d8a411a0851b80f2d757e05ee58ed
pranavkm21 <74977763+pranavkm21@users.noreply.github.com>
2025-04-12 20:23:04 +0530

    Update build.js

--- build.js
+++ build.js
@@ -2,13 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const ncp = require('ncp').ncp;
 
-const sourceDir = path.join(__dirname, '');  
+const sourceDir = path.join(__dirname, '');
 const distDir = path.join(__dirname, 'dist');
 
 // Check if dist folder exists; if not, create it
-if (!fs.existsSync(distDir)) {
-  fs.mkdirSync(distDir);
+if (fs.existsSync(distDir)) {
+  fs.rmSync(distDir, { recursive: true, force: true });  // Clear out any previous dist folder to avoid the nesting issue
 }
+fs.mkdirSync(distDir);
 
 // Copy all files from src to dist
 ncp(sourceDir, distDir, function (err) {
